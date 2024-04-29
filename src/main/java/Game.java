feaad6e83881e3db8f0636b703c753c1f1b8866c
jungmin <gmlsajfl93@gmail.com>
2024-04-29 11:13:23 +0900

    [feature] 52

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,5 +1,15 @@
 public class Game {
-    public void guess(String guessNumber) {
+    public String question;
+
+    public GuessResult guess(String guessNumber) {
+        assertIllegalArgument(guessNumber);
+        if(guessNumber.equals(question)){
+            return new GuessResult(true, 3, 0);
+        }
+        return null;
+    }
+
+    private void assertIllegalArgument(String guessNumber) {
         if (guessNumber == null) {
             throw new IllegalArgumentException();
         }
@@ -11,10 +21,14 @@ public class Game {
                 throw new IllegalArgumentException();
             }
         }
-        if(guessNumber.charAt(0) == guessNumber.charAt(1)
-        || guessNumber.charAt(1) == guessNumber.charAt(2)
-        || guessNumber.charAt(2) == guessNumber.charAt(0)){
+        if(isDuplicatedNumber(guessNumber)){
             throw new IllegalArgumentException();
         }
+    }
+
+    private boolean isDuplicatedNumber(String guessNumber) {
+        return guessNumber.charAt(0) == guessNumber.charAt(1)
+                || guessNumber.charAt(1) == guessNumber.charAt(2)
+                || guessNumber.charAt(2) == guessNumber.charAt(0);
     }
 }
